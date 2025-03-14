0291e222eb63cff0a23c780ad4926597439e4a58
edoubart <edouard.barthelemy@gmail.com>
2025-03-14 16:50:26 +0100

    Add References and Borrowing.

---
+++ 7-references-and-borrowing.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let mut trip: String = start_trip();
+
+    visit_philadelphia(&mut trip);
+
+    trip.push_str(" and ");
+
+    visit_new_york(&mut trip);
+
+    trip.push_str(" and ");
+
+    visit_boston(&mut trip);
+
+    show_itinerary(&trip);
+
+    /*
+     * Functions
+     */
+    fn start_trip() -> String {
+        String::from("The plan is...")
+    }
+
+    fn visit_philadelphia(trip: &mut String) {
+        trip.push_str("Philadelphia");
+    }
+
+    fn visit_new_york(trip: &mut String) {
+        trip.push_str("New York");
+    }
+
+    fn visit_boston(trip: &mut String) {
+        trip.push_str("Boston.");
+    }
+
+    fn show_itinerary(trip: &String) {
+        println!("Itinerary: {}", trip);
+    }
+}
