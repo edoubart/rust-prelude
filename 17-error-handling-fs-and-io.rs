9e56da602e6816a314b51f555805a3b5bc85435f
edoubart <edouard.barthelemy@gmail.com>
2025-03-21 18:10:22 +0100

    Leverage the  Associated Function.

--- 17-error-handling-fs-and-io.rs
+++ 17-error-handling-fs-and-io.rs
@@ -1,6 +1,6 @@
 // Cargo Crates
-use std::fs::File;
-use std::io::{self, stdin, Read};
+use std::fs;
+use std::io::{self, stdin};
 use std::process;
 
 fn main() {
@@ -21,50 +21,6 @@ fn read_file() -> Result<String, io::Error> {
     let mut input: String = String::new();
 
     stdin().read_line(&mut input)?;
-    //let user_requested_file: Result<usize, io::Error> = stdin()
-    //    .read_line(&mut input);
 
-    //if let Err(error) = user_requested_file {
-    //    return Err(error); // Equivalent to Result::Err(error)
-    //    //eprintln!(
-    //    //    "Something went wrong collecting user input. The error was {:#?}",
-    //    //    error
-    //    //);
-    //    //process::exit(1)
-    //}
-
-    let mut file_contents: String = String::new();
-    // Leverage the ? Operator (The Try Operator), and chain operations.
-    File::open(input.trim())? // -> Returns a Result<T>
-        .read_to_string(&mut file_contents)?; // -> Returns a Result<T>
-
-    //let mut file: File = File::open(input.trim())?;
-    //let mut file: File = match File::open(input.trim()) {
-    //    Ok(file) => file,
-    //    Err(error) => {
-    //        return Err(error); // Equivalent to Result::Err(error)
-    //        //eprintln!(
-    //        //    "Something went wrong opening the file. The error was {:#?}",
-    //        //    error
-    //        //);
-    //        //process::exit(1)
-    //    }
-    //};
-
-    //let mut file_contents: String = String::new();
-    //file.read_to_string(&mut file_contents)?;
-    //let read_operation: Result<usize, io::Error> = file
-    //    .read_to_string(&mut file_contents);
-
-    //if let Err(error) = read_operation {
-    //    return Err(error); // Equivalent to Result::Err(error)
-    //    //eprintln!(
-    //    //    "Something went wrong reading the file as a string. The error was {:#?}",
-    //    //    error
-    //    //);
-    //    //process::exit(1)
-    //}
-
-    Ok(file_contents)
-    //println!("{file_contents}");
+    fs::read_to_string(input.trim())
 }
