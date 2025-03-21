680f0c0c079842887dc6112699059ad649fb0f9a
edoubart <edouard.barthelemy@gmail.com>
2025-03-21 11:36:18 +0100

    Separate (decouple) the reading of the file from the handling of the result, and propagate (bubble up) errors.

--- 17-error-handling-fs-and-io.rs
+++ 17-error-handling-fs-and-io.rs
@@ -1,21 +1,34 @@
 // Cargo Crates
 use std::fs::File;
-use std::io::{stdin, Error, Read};
+use std::io::{self, stdin, Read};
 use std::process;
 
 fn main() {
+    let file_result: Result<String, io::Error> = read_file();
+
+    match file_result {
+        Ok(contents) => println!("{contents}"),
+        Err(error) => {
+            eprintln!("There was an error: {:#?}", error);
+            //process::exit(1)
+        }
+    }
+}
+
+fn read_file() -> Result<String, io::Error> {
     println!("Please enter the name of the file you'd like to read:");
     let mut input: String = String::new();
 
-    let user_requested_file: Result<usize, Error> = stdin()
+    let user_requested_file: Result<usize, io::Error> = stdin()
         .read_line(&mut input);
 
     if let Err(error) = user_requested_file {
-        eprintln!(
-            "Something went wrong collecting user input. The error was {:#?}",
-            error
-        );
-        process::exit(1);
+        return Err(error); // Equivalent to Result::Err(error)
+        //eprintln!(
+        //    "Something went wrong collecting user input. The error was {:#?}",
+        //    error
+        //);
+        //process::exit(1)
     }
 
     println!("input: {:#?}", input);
@@ -23,25 +36,28 @@ fn main() {
     let mut file: File = match File::open(input.trim()) {
         Ok(file) => file,
         Err(error) => {
-            eprintln!(
-                "Something went wrong opening the file. The error was {:#?}",
-                error
-            );
-            process::exit(1);
+            return Err(error); // Equivalent to Result::Err(error)
+            //eprintln!(
+            //    "Something went wrong opening the file. The error was {:#?}",
+            //    error
+            //);
+            //process::exit(1)
         }
     };
 
     let mut file_contents: String = String::new();
-    let read_operation: Result<usize, Error> = file
+    let read_operation: Result<usize, io::Error> = file
         .read_to_string(&mut file_contents);
 
     if let Err(error) = read_operation {
-        eprintln!(
-            "Something went wrong reading the file as a string. The error was {:#?}",
-            error
-        );
-        process::exit(1);
+        return Err(error); // Equivalent to Result::Err(error)
+        //eprintln!(
+        //    "Something went wrong reading the file as a string. The error was {:#?}",
+        //    error
+        //);
+        //process::exit(1)
     }
 
-    println!("{file_contents}");
+    Ok(file_contents)
+    //println!("{file_contents}");
 }
