142933f43282dcee818a699b4db35a5665e126bf
edoubart <edouard.barthelemy@gmail.com>
2025-03-22 16:50:19 +0100

    Add where clause.

--- 18-booking/src/main.rs
+++ 18-booking/src/main.rs
@@ -86,29 +86,39 @@ fn book_for_one_night<T: Accommodation>(entity: &mut T, guest: &str) {
 }
 
 // Option 1:
-fn mix_and_match(
-    // The first parameter will be a mutable reference to some type that
-    // implements both, Accommodation and Description traits.
-    first: &mut (impl Accommodation + Description),
-    // The second parameter promises to implement only the Accommodation trait.
-    second: &mut impl Accommodation,
-    guest: &str
-) {
-    first.book(guest, 1);
-    first.get_description();
-
-    second.book(guest, 1);
-}
+//fn mix_and_match(
+//    // The first parameter will be a mutable reference to some type that
+//    // implements both, Accommodation and Description traits.
+//    first: &mut (impl Accommodation + Description),
+//    // The second parameter promises to implement only the Accommodation trait.
+//    second: &mut impl Accommodation,
+//    guest: &str,
+//) {
+//    first.book(guest, 1);
+//    first.get_description();
+//
+//    second.book(guest, 1);
+//}
 
 // Option 2:
 //fn mix_and_match<T: Accommodation + Description, U: Accommodation>(
 //    first: &mut T,
 //    second: &mut U,
-//    guest: &str
+//    guest: &str,
 //) {
 //    first.book(guest, 1);
 //    second.book(guest, 1);
 //}
+
+// Option 3 (where clause):
+fn mix_and_match<T, U>(first: &mut T, second: &mut U, guest: &str)
+where
+    T: Accommodation + Description,
+    U: Accommodation,
+{
+    first.book(guest, 1);
+    second.book(guest, 1);
+}
 
 fn main() {
     let mut hotel: Hotel = Hotel::new("The Luxe");
