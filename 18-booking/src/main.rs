581bcc79623d86ff123fa10c58d328068f94859d
edoubart <edouard.barthelemy@gmail.com>
2025-03-22 17:41:14 +0100

    Add preview of trait objects.

--- 18-booking/src/main.rs
+++ 18-booking/src/main.rs
@@ -131,14 +131,18 @@ fn choose_best_place_to_stay() -> impl Accommodation + Description {
 }
 
 fn main() {
-    let hotel_1: Hotel<String> = Hotel::new(String::from("The Luxe"));
-    println!("{}", hotel_1.summarize());
+    let mut hotel: Hotel<String> = Hotel::new(String::from("The Luxe"));
+    let mut airbnb: AirBnB = AirBnB::new("Peter");
 
-    let hotel_2: Hotel<&str> = Hotel::new("The Golden Standard");
-    println!("{}", hotel_2.summarize());
+    // Dynamic Dispatch (at runtime). Only work with references.
+    // We refer to `&hotel` and `&airbnb` as dynamic Trait Objects.
+    let stays: Vec<&dyn Description> = vec![&hotel, &airbnb];
+    println!("{}", stays[0].get_description());
+    println!("{}", stays[1].get_description());
 
-    let hotel_3: Hotel<Vec<&str>> = Hotel::new(
-        vec!["The Sweet Escape", "Hilton Edition"]
-    );
-    //println!("{}", hotel_3.summarize()); // Doesn't work!
+    let mut stays: Vec<&mut dyn Accommodation> = vec![&mut hotel, &mut airbnb];
+    stays[0].book("Piers", 2);
+    stays[1].book("Amanda", 3);
+    println!("{:#?}", hotel);
+    println!("{:#?}", airbnb);
 }
