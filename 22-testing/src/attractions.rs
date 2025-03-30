c1e89fcd1388af5f0546c4e7da1bebb4fa45bdee
edoubart <edouard.barthelemy@gmail.com>
2025-03-30 16:14:04 +0200

    Modularize attractions.

---
+++ 22-testing/src/attractions.rs
@@ -0,0 +1,233 @@
+/*
+ * Traits
+ */
+trait TicketSeller {
+    fn sell_ticket(&mut self);
+}
+
+/*
+ * Structs
+ */
+#[derive(Debug, Eq, PartialEq)]
+struct Museum {
+    paintings: Vec<String>,
+    revenue: u32,
+}
+
+impl Museum {
+    const MAXIMUM_CAPACITY: usize = 3;
+
+    fn new() -> Self {
+        Self {
+            paintings: vec![],
+            revenue: 0,
+        }
+    }
+
+    fn buy_painting(&mut self, painting: &str) {
+        if self.paintings.len() >= Self::MAXIMUM_CAPACITY {
+            panic!("Museum does not have storage space for another painting!");
+        }
+
+        self.paintings.push(painting.to_string());
+    }
+
+    fn has_impressive_collection(&self) -> bool {
+        self.paintings.len() > 2
+    }
+}
+
+impl TicketSeller for Museum {
+    fn sell_ticket(&mut self) {
+        self.revenue += 25;
+    }
+}
+
+#[derive(Debug)]
+struct MovieTheatre {
+    movies: Vec<String>,
+    sales: u32,
+}
+
+impl MovieTheatre {
+    fn new() -> Self {
+        Self {
+            movies: vec![],
+            sales: 0,
+        }
+    }
+
+    fn add_movie(&mut self, movie: &str) {
+        self.movies.push(movie.to_string());
+    }
+}
+
+impl TicketSeller for MovieTheatre {
+    fn sell_ticket(&mut self) {
+        self.sales += 15;
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    /*
+     * To **test** is "to check the quality, performance, or reliability of
+     * something".
+     *
+     * **Tests** are code that validates that our program code works as
+     * expected.
+     *
+     * An **assertion** is a verification that a statement is valid. To
+     * **assert** means to state a fact.
+     *
+     * A **regression** is a bug introduced into working software.
+     *
+     * A test is a plain Rust function annotated with a `#[test]` attribute.
+     *
+     * A **unit test** targets a small component of a program in isolation.
+     *
+     * An **integration test** tests the interaction of multiple components within
+     * the program.
+     *
+     * The **`assert!`** macro validates that some condition or value is true.
+     *
+     * The **`assert_ne!`** macro validates that two values are not equal.
+     */
+
+    /*
+     * Cargo Crates
+     */
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    //#[test]
+    //fn print_success() {
+    //    // The following doesn't get printed as the test passes.
+    //    println!("Success inside the function");
+    //    assert!(true);
+    //}
+
+    //#[test]
+    //fn print_failure() {
+    //    println!("Failure inside the function");
+    //    assert!(false);
+    //}
+
+    //#[test]
+    //fn result_example() -> Result<(), String> {
+    //    Ok(())
+    //    //Err(String::from("Failure"))
+    //}
+
+    //#[test]
+    //fn hashmaps() {
+    //    let mut one: std::collections::HashMap<&str, i32> =
+    //        std::collections::HashMap::new();
+    //    one.insert("A", 2);
+    //    one.insert("B", 3);
+
+    //    let mut two: std::collections::HashMap<&str, i32> =
+    //        std::collections::HashMap::new();
+    //    two.insert("B", 3);
+    //    two.insert("C", 4);
+
+    //    assert_eq!(one, two);
+    //}
+
+    #[test]
+    // Classic Test functions version:
+    //pub fn museum_sells_ticket_to_increase_revenue() {
+    //    let mut museum: Museum = Museum::new();
+    //    museum.sell_ticket();
+    //    assert_eq!(
+    //        museum.revenue,
+    //        25,
+    //        "The revenue from selling 1 ticket did not match expectations."
+    //    ); // Better test
+    //    //assert_ne!(museum.revenue, 0); // Worse test
+    //}
+
+    // Result Enum version:
+    pub fn museum_sells_ticket_to_increase_revenue() -> Result<(), String> {
+        let mut museum: Museum = Museum::new();
+        museum.sell_ticket();
+
+        if museum.revenue == 25 {
+            Ok(())
+        } else {
+            Err(
+                String::from(
+                    "The revenue from selling 1 ticket did not match expectations."
+                )
+            )
+        }
+    }
+
+    #[test]
+    pub fn museum_can_sell_mutiple_tickets() {
+        let mut museum: Museum = Museum::new();
+        museum.sell_ticket();
+        museum.sell_ticket();
+        assert_eq!(museum.revenue, 50);
+        //assert_eq!(museum.revenue, 25); // Test fails!
+    }
+
+    #[test]
+    // Classic Test functions version:
+    //pub fn museum_can_have_impressive_collection() {
+    //    let mut museum: Museum = Museum::new();
+    //    museum.buy_painting("Mona Lisa");
+    //    museum.buy_painting("The Starry Night");
+    //    museum.buy_painting("Girl with a Pearl Earring");
+    //    assert!(
+    //        museum.has_impressive_collection(),
+    //        "The museum did not have an impressive collection despite having more than 2 paintings."
+    //    );
+    //}
+
+    // Result Enum version:
+    pub fn museum_can_have_impressive_collection() -> Result<(), String> {
+        let mut museum: Museum = Museum::new();
+        museum.buy_painting("Mona Lisa");
+        museum.buy_painting("The Starry Night");
+        museum.buy_painting("Girl with a Pearl Earring");
+
+        if museum.has_impressive_collection() {
+            Ok(())
+        } else {
+            Err(
+                String::from(
+                    "The museum did not have an impressive collection despite having more than 2 paintings."
+                )
+            )
+        }
+    }
+
+    #[test]
+    //#[ignore] // Don't add that to final test suite!
+    pub fn new_museums_are_equal() {
+        let museum_1: Museum = Museum::new();
+        let museum_2: Museum = Museum::new();
+        assert_eq!(
+            museum_1,
+            museum_2,
+            "Two new Museum instances were not found to be equal: {museum_1:?} // {museum_2:?}"
+        );
+
+        //let mut museum_1: Museum = Museum::new();
+        //museum_1.sell_ticket();
+        //let mut museum_2: Museum = Museum::new();
+        //assert_eq!(museum_1, museum_2);
+    }
+
+    #[test]
+    #[should_panic(expected = "storage space")]
+    pub fn museum_prohibits_adding_painting_when_capacity_has_been_reached() {
+        let mut museum: Museum = Museum::new();
+        museum.buy_painting("Mona Lisa");
+        museum.buy_painting("The Starry Night");
+        museum.buy_painting("Girl with a Pearl Earring");
+        museum.buy_painting("Water Lilies"); // Should panic here!
+    }
+}
+
